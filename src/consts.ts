843c3e58b3986d72bdc220d1b7c640ddd4611392
Nicholas Culpin <njculpin@gmail.com>
2023-07-31 17:04:50 -0700

    Revert "clean up"
    This reverts commit 6626347605b849142a002e4787255c8c6cb0b4ab.

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Nick Culpin";
-export const SITE_DESCRIPTION =
-  "Founder, Software Engineer, Game Designer, Artist";
+export const SITE_TITLE = 'My personal website.';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
