bc5682fb5bd99913665c7f47b771562f04e41a93
Nicholas Culpin <njculpin@gmail.com>
2024-08-21 15:30:47 -0700

    vector images

--- app/routes/vector-city-racers/index.tsx
+++ app/routes/vector-city-racers/index.tsx
@@ -13,35 +13,54 @@ export default function Index() {
       />
       <div className="font-sans p-4 flex flex-col justify-center items-center">
         <h1 className="text-3xl">{config.title}</h1>
-        <div className="mt-8">
+        <div className="mt-8 grid grid-cols-1 md:grid-cols-3 gap-8">
+          <img
+            alt={`vector_gm_car`}
+            src={"/vector_gm_car.jpg"}
+            sizes="(max-width: 500px) 100px, 600px"
+          />
+          <img
+            alt={`vector_hs_car`}
+            src={"/vector_hs_car.jpg"}
+            sizes="(max-width: 500px) 100px, 600px"
+          />
+          <img
+            alt={`vector_mb_car`}
+            src={"/vector_mb_car.jpg"}
+            sizes="(max-width: 500px) 100px, 600px"
+          />
+          <img
+            alt={`vector_od_car`}
+            src={"/vector_od_car.jpg"}
+            sizes="(max-width: 500px) 100px, 600px"
+          />
+          <img
+            alt={`vector_ol_car`}
+            src={"/vector_ol_car.jpg"}
+            sizes="(max-width: 500px) 100px, 600px"
+          />
+        </div>
+        <div className="mt-8 grid grid-cols-1 md:grid-cols-3 gap-8">
           <img
             alt={`vector_1`}
             src={"/vector_GM_Colors.jpg"}
             sizes="(max-width: 500px) 100px, 600px"
           />
-        </div>
-        <div className="mt-8">
           <img
             alt={`vector_2`}
             src={"/vector_MB_Colors.jpg"}
             sizes="(max-width: 500px) 100px, 600px"
           />
-        </div>
-        <div className="mt-8">
           <img
             alt={`vector_3`}
             src={"/vector_OL_Colors.jpg"}
             sizes="(max-width: 500px) 100px, 600px"
           />
-        </div>
-        <div className="mt-8">
           <img
             alt={`vector_4`}
             src={"/vector_OS_Colors.jpg"}
             sizes="(max-width: 500px) 100px, 600px"
           />
-        </div>
-        <div className="mt-8">
           <img
             alt={`vector_4`}
             src={"/vector_SS_Colors.jpg"}
