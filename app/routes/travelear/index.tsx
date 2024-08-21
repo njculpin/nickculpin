e7f65ac3b9785c5a300c5d4135171bb54fb08c09
Nicholas Culpin <njculpin@gmail.com>
2024-08-21 15:06:18 -0700

    zazzle and travelear images

--- app/routes/travelear/index.tsx
+++ app/routes/travelear/index.tsx
@@ -13,6 +13,20 @@ export default function Index() {
       />
       <div className="font-sans p-4 flex flex-col justify-center items-center">
         <h1 className="text-3xl">{config.title}</h1>
+        <div className="mt-8 w-full md:w-1/2">
+          <img
+            alt={`travelear`}
+            src={"/travelear_1.jpg"}
+            sizes="(max-width: 500px) 100px, 600px"
+          />
+        </div>
+        <div className="mt-8 w-full md:w-1/2">
+          <img
+            alt={`dreamear`}
+            src={"/dreamear_1.jpg"}
+            sizes="(max-width: 500px) 100px, 600px"
+          />
+        </div>
       </div>
     </div>
   );
--- app/routes/zazzle/index.tsx
+++ app/routes/zazzle/index.tsx
@@ -13,6 +13,31 @@ export default function Index() {
       />
       <div className="font-sans p-4 flex flex-col justify-center items-center">
         <h1 className="text-3xl">{config.title}</h1>
+        <div className="mt-8">
+          <img
+            alt={`zazzle`}
+            src={"/zazzle_work.jpg"}
+            sizes="(max-width: 500px) 100px, 600px"
+          />
+        </div>
+        <div className="mt-8">
+          <img
+            alt={`zazzle`}
+            src={"/zazzle_talus.jpg"}
+            sizes="(max-width: 500px) 100px, 600px"
+          />
+        </div>
+        <div className="mt-8">
+          <img
+            alt={`zazzle`}
+            src={"/zazzle_photogram.jpg"}
+            sizes="(max-width: 500px) 100px, 600px"
+          />
+        </div>
+        <div className="mt-8 grid grid-cols-1 md:grid-cols-2 gap-8">
+          <video src={"/zazzle_talus_video_1.MOV"} width="600" controls />
+          <video src={"/zazzle_talus_video_2.MOV"} width="600" controls />
+        </div>
       </div>
     </div>
   );
