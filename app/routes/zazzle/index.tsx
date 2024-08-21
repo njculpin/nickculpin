8d5ade61c7f441747a5d166af311e743830c4121
Nicholas Culpin <njculpin@gmail.com>
2024-08-21 16:56:07 -0700

    styles

--- app/routes/zazzle/index.tsx
+++ app/routes/zazzle/index.tsx
@@ -11,32 +11,34 @@ export default function Index() {
       <Bread
         pages={[{ name: config.title, href: config.href, current: true }]}
       />
-      <div className="font-sans p-4 flex flex-col justify-center items-center">
-        <h1 className="text-3xl">{config.title}</h1>
-        <div className="mt-8">
-          <img
-            alt={`zazzle`}
-            src={"/zazzle_work.jpg"}
-            sizes="(max-width: 500px) 100px, 600px"
-          />
-        </div>
-        <div className="mt-8">
-          <img
-            alt={`zazzle`}
-            src={"/zazzle_talus.jpg"}
-            sizes="(max-width: 500px) 100px, 600px"
-          />
-        </div>
-        <div className="mt-8">
-          <img
-            alt={`zazzle`}
-            src={"/zazzle_photogram.jpg"}
-            sizes="(max-width: 500px) 100px, 600px"
-          />
-        </div>
-        <div className="mt-8 grid grid-cols-1 md:grid-cols-2 gap-8">
-          <video src={"/zazzle_talus_video_1.MOV"} width="600" controls />
-          <video src={"/zazzle_talus_video_2.MOV"} width="600" controls />
+      <div className="flex justify-center items-center">
+        <div className="max-w-7xl font-sans p-4 flex flex-col justify-center items-center">
+          <h1 className="text-3xl">{config.title}</h1>
+          <div className="mt-8">
+            <img
+              alt={`zazzle`}
+              src={"/zazzle_work.jpg"}
+              sizes="(max-width: 500px) 100px, 600px"
+            />
+          </div>
+          <div className="mt-8">
+            <img
+              alt={`zazzle`}
+              src={"/zazzle_talus.jpg"}
+              sizes="(max-width: 500px) 100px, 600px"
+            />
+          </div>
+          <div className="mt-8">
+            <img
+              alt={`zazzle`}
+              src={"/zazzle_photogram.jpg"}
+              sizes="(max-width: 500px) 100px, 600px"
+            />
+          </div>
+          <div className="mt-8 grid grid-cols-1 md:grid-cols-2 gap-8">
+            <video src={"/zazzle_talus_video_1.MOV"} width="600" controls />
+            <video src={"/zazzle_talus_video_2.MOV"} width="600" controls />
+          </div>
         </div>
       </div>
     </div>
