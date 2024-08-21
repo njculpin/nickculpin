3518762e3543c66a21abfa62bc1b7f404f6a4414
Nicholas Culpin <njculpin@gmail.com>
2024-08-21 15:21:41 -0700

    vcr

--- app/routes/vector-city-racers/index.tsx
+++ app/routes/vector-city-racers/index.tsx
@@ -13,6 +13,41 @@ export default function Index() {
       />
       <div className="font-sans p-4 flex flex-col justify-center items-center">
         <h1 className="text-3xl">{config.title}</h1>
+        <div className="mt-8">
+          <img
+            alt={`vector_1`}
+            src={"/vector_GM_Colors.jpg"}
+            sizes="(max-width: 500px) 100px, 600px"
+          />
+        </div>
+        <div className="mt-8">
+          <img
+            alt={`vector_2`}
+            src={"/vector_MB_Colors.jpg"}
+            sizes="(max-width: 500px) 100px, 600px"
+          />
+        </div>
+        <div className="mt-8">
+          <img
+            alt={`vector_3`}
+            src={"/vector_OL_Colors.jpg"}
+            sizes="(max-width: 500px) 100px, 600px"
+          />
+        </div>
+        <div className="mt-8">
+          <img
+            alt={`vector_4`}
+            src={"/vector_OS_Colors.jpg"}
+            sizes="(max-width: 500px) 100px, 600px"
+          />
+        </div>
+        <div className="mt-8">
+          <img
+            alt={`vector_4`}
+            src={"/vector_SS_Colors.jpg"}
+            sizes="(max-width: 500px) 100px, 600px"
+          />
+        </div>
       </div>
     </div>
   );
