b1b2247691ed4888cb195d941ea17632b80f1fd5
Nicholas Culpin <njculpin@gmail.com>
2024-08-29 15:13:27 -0700

    loader

--- app/games/lights-out/index.tsx
+++ app/games/lights-out/index.tsx
@@ -1,10 +1,9 @@
 import type { MetaFunction } from "@remix-run/node";
 import { Vector3 } from "three";
 import { MyCanvas } from "~/components/canvas";
-import { MapControls } from "@react-three/drei";
+import { MapControls, Loader } from "@react-three/drei";
 import { LightsOutGame } from "./LightsOutGame";
 import { Socials } from "~/components/socials";
-import { Loading } from "~/components/loading";
 import { Suspense } from "react";
 
 export const meta: MetaFunction = () => {
@@ -21,11 +20,12 @@ export default function LightsOut() {
   return (
     <div className="h-screen w-screen bg-zinc-900">
       <MyCanvas>
-        <Suspense fallback={<Loading />}>
+        <Suspense fallback={null}>
           <MapControls enableZoom={false} enableRotate={false} />
           <LightsOutGame position={new Vector3(0, 0, 0)} />
         </Suspense>
       </MyCanvas>
+      <Loader />
       <div className="absolute z-10 bottom-4 right-4">
         <Socials />
       </div>
